bb9b2d6c902ba59a258d9991ac12c5b5527f7c8b
Alexandra <alexadler@bk.ru>
2022-06-21 00:06:30 +0500

    Homework done

--- src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -1,13 +1,33 @@
 package ru.hogwarts.school;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import ru.hogwarts.school.controller.AvatarController;
+import ru.hogwarts.school.controller.FacultyController;
+import ru.hogwarts.school.controller.StudentController;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SchoolApplicationTests {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private AvatarController avatarController;
+
+	@Autowired
+	private FacultyController facultyController;
+
+	@Autowired
+	private StudentController studentController;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		Assertions.assertThat(avatarController).isNotNull();
+		Assertions.assertThat(facultyController).isNotNull();
+		Assertions.assertThat(studentController).isNotNull();
 	}
 
 }
